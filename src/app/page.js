42f7116d5e63d48d9336c4b8686c066f62db8557
sagites <chizzyikemba@gmail.com>
2025-04-30 18:09:13 +0100

    just for trial and error

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+'use client';
 import ButtonPrimary from "@/components/buttons/buttonPrimary";
 import ButtonSecondary from "@/components/buttons/buttonSecondary";
+import InputField from "@/components/inputs/textField";
 import React from "react";
 
 const Home = () => {
@@ -7,6 +9,7 @@ const Home = () => {
     <div>
       <ButtonPrimary label="Helllooo" width="auto"/>
       <ButtonSecondary label="Hello again"/>
+      <InputField type='email'  placeholder="Enter your email"/>
     </div>
   );
 };
