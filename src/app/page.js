39b693c515af1afe76c0435be0d807f573cf3f68
sagites <chizzyikemba@gmail.com>
2025-05-01 14:20:48 +0100

    Rando

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,8 @@
 'use client';
-import ButtonPrimary from "@/components/buttons/buttonPrimary";
-import ButtonSecondary from "@/components/buttons/buttonSecondary";
-import InputField from "@/components/inputs/textField";
-import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <ButtonPrimary label="Helllooo" width="auto"/>
-      <ButtonSecondary label="Hello again"/>
-      <InputField type='email'  placeholder="Enter your email"/>
     </div>
   );
 };
