232be762262c95cab5bae3688e6007608b9338ac
sagites <chizzyikemba@gmail.com>
2025-05-01 14:19:19 +0100

    Updated the buttons

--- src/components/buttons/buttonSecondary.jsx
+++ src/components/buttons/buttonSecondary.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-const ButtonSecondary = ({ label, onClick, type = "button", className}) => {
+const ButtonSecondary = ({
+  label,
+  onClick,
+  imageSrc,
+  type = "button",
+  classNameButton,
+  classNameLabel
+}) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`px-4 py-2 button text-Body-M bg-white  active:bg-lighterPurple text-purple  focus:ring-purple cursor-pointer disabled:text-lighterPurple disabled:ring-focus:ring disabled:ring-lighterPurple rounded-xl ${className}`}
+      className={`px-4 py-2 button text-Body-M bg-white border border-purple active:bg-lighterPurple text-purple  focus:ring-purple cursor-pointer disabled:text-lighterPurple disabled:ring-focus:ring disabled:ring-lighterPurple rounded-lg hover:bg-lighterPurple ${classNameButton}`}
     >
-      {label}
+      <img src={imageSrc} className="sm:hidden" alt="" />
+      <span className={`${classNameLabel}`}>{label}</span>
     </button>
   );
 };
